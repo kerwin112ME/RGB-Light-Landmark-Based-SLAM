78e78df4a1df52f9515a6b3f9c657587b3db7a4c
Kuan Wei Liu <40621758+kerwin112ME@users.noreply.github.com>
2020-04-11 12:11:50 -0500

    Update RunSLAM.m

--- RunSLAM.m
+++ RunSLAM.m
@@ -15,13 +15,9 @@ function PF = RunSLAM(Ut, RGBt, numP, lmY)
     %% initilize
     Q = zeros(3,3); % lm process noise 
     R = diag([0.5,0.5,0.5]); % lm measurement noise
-<<<<<<< HEAD
+
     mapL = 6; % size of the map
     lmX_hat = [2.2;3.0;3.8]; % initial guess of the landmark x-positions
-=======
-    mapL = 10; % size of the map
-    lmX_hat = [2.2;3.2;2.8]; % initial guess of the landmark x-positions
->>>>>>> d697f5f8672ab25dce4aac0d69630ea6a7f9f513
     
     tspan = size(Ut,2);
     PF = ParticleFilterSLAM(numP, mapL, lmY, Q, R); % declare a ParticleFilterSLAM
@@ -53,15 +49,13 @@ function PF = RunSLAM(Ut, RGBt, numP, lmY)
         
         PF = PF.resampling();
         
-        
-<<<<<<< HEAD
+
         % update the particles plot
-        
-=======
+
         %
         % update the particles plot
         %
->>>>>>> d697f5f8672ab25dce4aac0d69630ea6a7f9f513
+
         [est_x, est_y] = PF.computeLocation();
         plot(est_x,est_y,'o', 'color', '#FF00FF');
         hold on
@@ -74,16 +68,10 @@ function PF = RunSLAM(Ut, RGBt, numP, lmY)
         
         plot(scatter(1,:),scatter(2,:),'.','color','#D95319');
         hold on
-<<<<<<< HEAD
+
         plot(PF.lmX_est(1),lmY(1),'pentagram','MarkerSize',9,'LineWidth',3,'color','#FF0000') % plot Red Lm
         plot(PF.lmX_est(2),lmY(2),'pentagram','MarkerSize',9,'LineWidth',3,'color','#00FF00') % plot Green Lm
         plot(PF.lmX_est(3),lmY(3),'pentagram','MarkerSize',9,'LineWidth',3,'color','#0000FF') % plot Blue Lm
-=======
-        
-        plot(lm_init(1,1),lm_init(2,1),'pentagram','MarkerSize',9,'LineWidth',3,'color','#FF0000') % plot Red Lm
-        plot(lm_init(1,2),lm_init(2,2),'pentagram','MarkerSize',9,'LineWidth',3,'color','#00FF00') % plot Green Lm
-        plot(lm_init(1,3),lm_init(2,3),'pentagram','MarkerSize',9,'LineWidth',3,'color','#0000FF') % plot Blue Lm
->>>>>>> d697f5f8672ab25dce4aac0d69630ea6a7f9f513
         hold off;
         
         axis([0 mapL 0 mapL]);
